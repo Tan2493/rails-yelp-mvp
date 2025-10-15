d86c71e1b48813626416c2e1b2f12350e21e428c
Tania Kwok <tania@pinkmojito.net>
2025-10-15 21:49:00 +0400

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,20 +12,20 @@ puts "Cleaning database..."
 Restaurant.destroy_all
 
 puts "Creating restaurants..."
-Restaurant.create!(name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '0102030405', category: 'french')
-puts "Created Dishoom"
+Restaurant.create!(name: 'La Raclette', address: 'Curepipe', phone_number: '02030405', category: 'french')
+puts "Created La Raclette"
 
-Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0203040506", category: "italian")
-puts "Created Pizza East"
+Restaurant.create!(name: "Pizza Hut", address: "Grand Bay", phone_number: "72459076", category: "italian")
+puts "Created Pizza Hut"
 
-Restaurant.create!(name: "Sushi House", address: "10 Tokyo St", phone_number: "0304050607", category: "japanese")
-puts "Created Sushi House"
+Restaurant.create!(name: "Surf & Turf", address: "Pointe d'Esny", phone_number: "67040506", category: "japanese")
+puts "Created Surf & Turf"
 
-Restaurant.create!(name: "Dragon Wok", address: "45 Beijing Rd", phone_number: "0405060708", category: "chinese")
+Restaurant.create!(name: "Dragon Wok", address: "Port-Louis", phone_number: "50607089", category: "chinese")
 puts "Created Dragon Wok"
 
-Restaurant.create!(name: "Chez Belgique", address: "5 Brussels Ln", phone_number: "0506070809", category: "belgian")
-puts "Created Chez Belgique"
+Restaurant.create!(name: "Moules Frites", address: "Calodyne", phone_number: "06070809", category: "belgian")
+puts "Created Moules Frites"
 
 # 3. Display a message 🎉
 puts "Finished! Created #{Restaurant.count} restaurants."
