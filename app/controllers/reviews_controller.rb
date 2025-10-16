e078f6a0614ed2924925f0528aa982e229c4d9f5
Tania Kwok <tania@pinkmojito.net>
2025-10-16 22:08:04 +0400

    Yelp MVP

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,26 @@
 class ReviewsController < ApplicationController
-  def index
-    @reviews = Review.all
-  end
+  before_action :set_restaurant, only: %i[new create]
 
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    @review.save
-    redirect_to review_path(@review)
-  end
-
-  def show
-    @review = Review.find(params[:id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
